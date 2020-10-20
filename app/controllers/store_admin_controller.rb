5188fb3541aaca7a045627b9924a33be4b6b97f7
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-20 18:04:03 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,16 +1,16 @@
 class StoreAdminController < ApplicationController
 
-  layout 'admin'
+    layout "admin"
 
-  def home
-  end
+    def home
+    end 
 
-  def orders
-    render :layout => "order_administration"
-  end
+    def orders
+        render :layout => "order_administration"
+    end 
 
-  def invoice
-    render layout: false
-  end
+    def invoice
+        render :layout => false 
+    end 
 
-end 
+end  
